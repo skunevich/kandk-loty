f07adb6c6460057a643e44f3ff331920341534cb
ekerwin <ekerwin@kingofnerds.com>
2010-05-23 06:30:50 -0700

    just some minor syntax refactoring

--- karate_chop_skunevich/KarateChop.rb
+++ karate_chop_skunevich/KarateChop.rb
@@ -1,30 +1,30 @@
 	def chop(int, array_of_int)
-		if array_of_int.size == 0 then
-			return -1;
+		if array_of_int.size == 0
+			-1;
 		end
-		return chopWithPosition(int, array_of_int, 0, array_of_int.size-1)
+		chopWithPosition(int, array_of_int, 0, array_of_int.size-1)
 	end
 
 	def chopWithPosition(int, array_of_int, startPosition, endPosition)
-		if startPosition > endPosition then
-			return -1;
+		if startPosition > endPosition
+			-1;
 		end
-		if startPosition == endPosition then
-			if array_of_int.at(startPosition) == int then
-				return startPosition
+		if startPosition == endPosition
+			if array_of_int[startPosition] == int
+				startPosition
 			else
-				return -1;
+				-1;
 			end
 		end
 		
 		middle = (startPosition + endPosition) / 2
-		value = array_of_int.at(middle)
+		value = array_of_int[middle]
 		if value == int then
-			return middle
+			middle
 		end
 		if value > int
-			return chopWithPosition(int, array_of_int, startPosition, middle)
+			chopWithPosition(int, array_of_int, startPosition, middle)
 		else
-			return chopWithPosition(int, array_of_int, middle + 1, endPosition)
+			chopWithPosition(int, array_of_int, middle + 1, endPosition)
 		end
 	end
