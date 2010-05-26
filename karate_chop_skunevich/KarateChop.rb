4d7df281926f8ee5d8888bcf56d8e15d15f843b3
skunevich <sean.kunevich@gmail.com>
2010-05-26 16:06:00 -0400

    syntax changes

--- karate_chop_skunevich/KarateChop.rb
+++ karate_chop_skunevich/KarateChop.rb
@@ -1,30 +1,30 @@
 	def chop(int, array_of_int)
-		if array_of_int.size == 0
+		if array_of_int.size == 0 
 			-1;
 		end
 		chopWithPosition(int, array_of_int, 0, array_of_int.size-1)
 	end
 
 	def chopWithPosition(int, array_of_int, startPosition, endPosition)
-		if startPosition > endPosition
-			-1;
+		if startPosition > endPosition 
+			return -1;
 		end
-		if startPosition == endPosition
-			if array_of_int[startPosition] == int
-				startPosition
+		if startPosition == endPosition 
+			if array_of_int.at(startPosition) == int 
+				return startPosition
 			else
-				-1;
+				return -1;
 			end
 		end
-		
+
 		middle = (startPosition + endPosition) / 2
-		value = array_of_int[middle]
-		if value == int then
-			middle
+		value = array_of_int.at(middle)
+		if value == int 
+			return middle
 		end
 		if value > int
-			chopWithPosition(int, array_of_int, startPosition, middle)
+			return chopWithPosition(int, array_of_int, startPosition, middle)
 		else
-			chopWithPosition(int, array_of_int, middle + 1, endPosition)
+			return chopWithPosition(int, array_of_int, middle + 1, endPosition)
 		end
 	end
